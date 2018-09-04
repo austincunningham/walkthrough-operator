ef3e21d08a476b9c99bf6a829f99a59d5facf9de
Michael Nairn <mnairn@redhat.com>
2018-09-04 10:51:32 +0100

    INITIAL COMMIT

---
+++ pkg/apis/integr8ly/v1alpha1/doc.go
@@ -0,0 +1,3 @@
+// +k8s:deepcopy-gen=package
+// +groupName=integr8ly.aerogear.org
+package v1alpha1
---
+++ pkg/apis/integr8ly/v1alpha1/types.go
@@ -0,0 +1,29 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type WalkthroughList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []Walkthrough `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type Walkthrough struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              WalkthroughSpec   `json:"spec"`
+	Status            WalkthroughStatus `json:"status,omitempty"`
+}
+
+type WalkthroughSpec struct {
+	// Fill me
+}
+type WalkthroughStatus struct {
+	// Fill me
+}
